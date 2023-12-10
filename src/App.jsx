b9aabd8b988fe3f71e0c7ec947021dbdd8f8cd68
roman.klemiato <roman.klemiato18@gmail.com>
2023-12-11 00:20:36 +0200

    added more styling to the project

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,29 @@ const Country = ({ country, getWeatherData, weather }) => {
   let languagesValues = Object.values(country[0].languages);
 
   return (
-    <>
-      <h1>{country[0].name.common}</h1>
-      <p>capital: {country[0].capital}</p>
-      <p>area: {country[0].area} </p>
+    <div className="country">
+      <div className="upper-container">
+        <img src={country[0].flags["png"]} width="220" />
+      </div>
+      <div className="lower-container">
+        <h1 className="name">{country[0].name.common}</h1>
+        <p className="capital">capital: {country[0].capital}</p>
+        <p className="area">area: {country[0].area} </p>
 
-      <h3>languages:</h3>
-      <ul>
-        {languagesValues.map((x, index) => {
-          return <li key={index}>{x}</li>;
-        })}
-      </ul>
-      <img src={country[0].flags["png"]} width="220" />
-      <Weather
-        country={country}
-        weather={weather}
-        getWeatherData={getWeatherData}
-      />
-    </>
+        <h3 className="languages">languages:</h3>
+        <ul className="languages-ul">
+          {languagesValues.map((x, index) => {
+            return <li key={index}>{x}</li>;
+          })}
+        </ul>
+
+        <Weather
+          country={country}
+          weather={weather}
+          getWeatherData={getWeatherData}
+        />
+      </div>
+    </div>
   );
 };
 
@@ -41,13 +46,15 @@ const Weather = ({ country, weather, getWeatherData }) => {
 
   return (
     <>
-      <h2>Weather in {country[0].name.common}</h2>
-      <p>temperature: {weather.main.temp} Celsius</p>
-      <img
-        src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-        width="120"
-      />
-      <p>wind: {weather.wind.speed} m/s</p>
+      <h2 className="weather">Weather in {country[0].name.common}</h2>
+      <p className="temperature">temperature: {weather.main.temp} Celsius</p>
+      <div className="weather-img">
+        <img
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          width="120"
+        />
+      </div>
+      <p className="wind">wind: {weather.wind.speed} m/s</p>
     </>
   );
 };
@@ -56,16 +63,25 @@ const Countries = ({ countries, setFilterString, weather, getWeatherData }) => {
   // user requires to be specific with the search, otherwise it returns an error message.
   if (countries.length > 10) {
     console.log(countries.length);
-    return <p>Too many matches, specify another filter</p>;
+    return (
+      <p className="too-many-matches">
+        Too many matches, specify another filter
+      </p>
+    );
   } else if (countries.length <= 10 && countries.length > 1) {
     return (
-      <ul>
+      <ul className="filter-country">
         {countries.map((country, index) => {
           return (
             <>
-              <li key={index}>
+              <li key={index} className="filter-country-li">
                 {country.name.common}{" "}
-                <button onClick={() => setFilterString(country.name.common)}>
+                <button
+                  onClick={() => {
+                    setFilterString(country.name.common);
+                  }}
+                  className="filter-btn"
+                >
                   show
                 </button>
               </li>
@@ -139,7 +155,7 @@ function App() {
   return (
     <>
       <div className="search-box">
-        <label>Weather API</label>
+        <label>Country Data</label>
         <input
           name="filterCountry"
           value={filterString}
